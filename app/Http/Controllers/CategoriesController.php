ddf75b7b77c5451189002eb715c609089310eb30
DouglasHT <doug.wow@hotmail.com>
2019-02-26 17:07:16 -0300

    Update 201902261707

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -2,15 +2,28 @@
 
 namespace CodeDelivery\Http\Controllers;
 
+
+use CodeDelivery\Http\Requests\AdminCategoryRequest;
 use CodeDelivery\Repositories\CategoryRepository;
+use Illuminate\Http\Request;
 
 
 class CategoriesController extends Controller
 {
-    public function index(CategoryRepository $repository)
+
+    private $repository;
+
+    public function __construct(CategoryRepository $repository)
+    {
+        $this->repository = $repository;
+
+    }
+
+
+    public function index()
     {
 
-        $categories = $repository->paginate(5);
+        $categories = $this->repository->paginate();
 
 
         return view('admin.categories.index', compact('categories'));
@@ -19,5 +32,26 @@ class CategoriesController extends Controller
     public function create()
     {
         return view('admin.categories.create');
+    }
+
+    public function store(AdminCategoryRequest $request)
+    {
+        $data = $request->all();
+        $this->repository->create($data);
+
+        return redirect()->route('admin.categories.index');
+    }
+
+    public function edit($id)
+    {
+        $category = $this->repository->find($id);
+
+        return view('admin.categories.edit', compact('category'));
+
+    }
+
+    public function update(AdminCategoryRequest $request, $id)
+    {
+
     }
 }
--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -7,6 +7,26 @@
 
         <h3>Nova categoria</h3>
 
+        @if($errors->any())
+            <ul class="alert">
+                @foreach($errors->all() as $error)
+                    <li>{{$error}}</li>
+                @endforeach
+            </ul>
+        @endif
+
+        {!! Form::open(['route'=>'admin.categories.store', 'class'=>'form']) !!}
+
+        <div class="form-group">
+            {!! Form::label('Name', 'Nome:') !!}
+            {!! Form::text('name', null, ['class'=>'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::submit('Criar categoria',['class'=>'btn btn-primary']) !!}
+        </div>
+
+        {!! Form::close() !!}
     </div>
 
 
