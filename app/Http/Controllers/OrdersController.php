7d1f3b8c13c049c71803b287ba463a7dca8f4c84
DouglasHT <doug.wow@hotmail.com>
2019-03-06 14:09:15 -0300

    Update 201903061409

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -4,8 +4,10 @@
 namespace CodeDelivery\Http\Controllers;
 
 
+
 use CodeDelivery\Repositories\OrderRepository;
 use CodeDelivery\Repositories\UserRepository;
+use Illuminate\Http\Request;
 
 class OrdersController extends Controller
 {
@@ -26,12 +28,22 @@ class OrdersController extends Controller
 
     public function edit($id, UserRepository $userRepository)
     {
-        $list_status = [0=>'Pendente', 1=>'A caminho', 2=>'Entregue'];
+        $list_status = [0=>'Pendente', 1=>'A caminho', 2=>'Entregue', 3=>'Cancelado'];
         $order = $this->repository->find($id);
 
         $deliveryman = $userRepository->getDeliveryman();
 
         return view('admin.orders.edit', compact('order','list_status', 'deliveryman'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $all = $request->all();
+
+        $this->repository->update($all, $id);
+
+        return redirect()->route('admin.orders.index');
+
     }
 
 }
