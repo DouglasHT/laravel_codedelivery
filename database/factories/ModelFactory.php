7ee7512f409594c7316be2d5233069af7305d8cf
DouglasHT <doug.wow@hotmail.com>
2019-03-07 11:16:07 -0300

    Update 201903071115

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -71,7 +71,6 @@ $factory->define(\CodeDelivery\Models\Cupom::class, function(Faker\Generator $fa
     return [
         'code' => rand(100, 10000),
         'value' => rand(50, 100),
-
     ];
 });
 
